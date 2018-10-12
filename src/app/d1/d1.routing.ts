93e513a2e6f8223097d53127425ed2099e03d4d4
ysrujana <y.devisrujana@gmail.com>
2018-10-12 17:19:54 +0530

    restored

--- src/app/d1/d1.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { D1Component } from './d1.component';
-
-export const d1Router: Routes = [
-  {
-    path: '',
-    component: D1Component
-  }
-];
--- src/app/d2/d2.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class D2Service {
-
-  constructor() { }
-}
