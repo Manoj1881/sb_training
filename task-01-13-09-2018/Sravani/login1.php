baefd5010f617b53ba13a4807124bdff76421401
sravanijarubula <45390257+sravanijarubula@users.noreply.github.com>
2018-11-30 17:24:29 +0530

    Add files via upload

---
+++ task-01-13-09-2018/Sravani/login1.php
@@ -0,0 +1,7 @@
+
+<html>
+<body>
+	welcome:<?php echo $_POST["firstname"]; ?>
+	<?php echo $_POST["lastname"]; ?>
+</body>
+</html>
---
+++ task-01-13-09-2018/Sravani/new 2.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+	welcome:<?php echo $_POST["firstname"]; ?>
+	<?php echo $_POST["lastname"]; ?>
+</body>
+</html>
